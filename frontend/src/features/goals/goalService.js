e0bc5d9db29c5c8b0d53f46e54dabd5c0084601b
fatmagh96 <98395704+fatmagh96@users.noreply.github.com>
2024-02-15 15:40:42 +0100

    goal item

--- frontend/src/features/goals/goalService.js
+++ frontend/src/features/goals/goalService.js
@@ -32,10 +32,25 @@ const getGoals = async (token)=>{
 
 }
 
+// delete goal
+const deleteGoal = async (goalId, token)=>{
+    const config = {
+        headers:{
+            Authorization: `Bearer ${token}` ,
+        }
+    }
+
+    const response = await axios.delete(API_URL + goalId, config)
+
+    return response.data
+
+}
+
 
 const goalService = {
     createGoal,
     getGoals,
+    deleteGoal,
 }
 
 export default goalService
